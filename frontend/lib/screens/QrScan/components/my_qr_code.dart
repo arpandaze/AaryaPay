415cd1fcc98e78822fc95cbf9f783a03d1a2a815
Rushab11 <humagainrushab@gmail.com>
2023-04-11 22:31:03 +0545

    feat: QR code scanner screen designed

---
+++ frontend/lib/screens/QrScan/components/my_qr_code.dart
@@ -0,0 +1,81 @@
+import 'dart:ffi' as ffi;
+import 'dart:typed_data';
+import 'dart:math' as math;
+import 'package:aaryapay/components/CustomTextField.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:aaryapay/screens/QrScan/qrscan_bloc.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class MyQRCode extends StatelessWidget {
+  const MyQRCode({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height * 0.5,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Container(
+          width: size.width,
+          height: 60,
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              color: Theme.of(context).colorScheme.primary),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 15),
+                child: Text(
+                  "My QR Code",
+                  style: Theme.of(context).textTheme.titleSmall!.merge(
+                        const TextStyle(color: Colors.white),
+                      ),
+                ),
+              ),
+              Transform.rotate(
+                angle: math.pi / 2,
+                child: IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: SvgPicture.asset('assets/icons/arrow3.svg',
+                      width: 20,
+                      height: 20,
+                      colorFilter: const ColorFilter.mode(
+                          Colors.white, BlendMode.srcIn)),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 20, bottom: 15),
+          child: QrImage(
+            data:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+            size: 250,
+          ),
+        ),
+        Text(
+          "AaryaPay Nepali",
+          style: Theme.of(context).textTheme.titleMedium!,
+        ),
+        Text(
+          "Scan QR Code to receive Money",
+          style: Theme.of(context).textTheme.bodyMedium!,
+          textAlign: TextAlign.center
+        )
+      ]),
+    );
+  }
+}
---
+++ frontend/lib/screens/QrScan/qrscan_screen.dart
@@ -0,0 +1,95 @@
+import 'dart:ffi' as ffi;
+import 'dart:typed_data';
+import 'dart:math' as math;
+import 'package:aaryapay/components/CustomTextField.dart';
+import 'package:aaryapay/screens/QrScan/components/bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:aaryapay/screens/QrScan/qrscan_bloc.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QrScanScreen extends StatelessWidget {
+  const QrScanScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return BlocProvider(
+      create: (context) => QrScannerBloc(),
+      child: Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SafeArea(
+            top: true,
+            bottom: true,
+            left: true,
+            right: true,
+            child: body(size, context),
+          )),
+    );
+  }
+
+  Widget body(Size size, BuildContext context) {
+    return BlocConsumer<QrScannerBloc, QrScannerState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      buildWhen: ((prev, next) => prev != next),
+      builder: (context, state) {
+        return Stack(
+          children: [
+            Container(
+              child: QRView(
+                key: context.read<QrScannerBloc>().qrKey,
+                onQRViewCreated: context.read<QrScannerBloc>().onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                    borderColor: Colors.white,
+                    borderRadius: 34,
+                    borderWidth: 10,
+                    cutOutSize: (size.width < 400 || size.height < 400)
+                        ? 150.0
+                        : 300.0),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      child: IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: SvgPicture.asset('assets/icons/close.svg',
+                            width: 20, height: 20, color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: Text(
+                    "Scan QR Code",
+                    style: Theme.of(context).textTheme.bodyLarge!.merge(
+                        const TextStyle(height: 1.5)
+                            .merge(const TextStyle(color: Colors.white))),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "to Pay",
+                    style: Theme.of(context).textTheme.bodyLarge!.merge(
+                        const TextStyle(height: 1.5)
+                            .merge(const TextStyle(color: Colors.white))),
+                  ),
+                ),
+              ],
+            ),
+            BottomQrBar(),
+          ],
+        );
+      },
+    );
+  }
+}
+
+
