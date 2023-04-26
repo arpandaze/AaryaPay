03bcd4f054a97eab1b1f8eb07e1c3273d3e56151
Daze <dazehere@yandex.com>
2023-04-26 18:34:41 +0545

    fix: sender and receiver constraint added to transaction table

--- api/migrations/templates/transactions.sql
+++ api/migrations/templates/transactions.sql
@@ -5,7 +5,8 @@ CREATE TABLE Transactions (
     amount NUMERIC(15,2) NOT NULL,
     generation_time TIMESTAMP NOT NULL,
     received_time TIMESTAMP NOT NULL,
-    verification_time TIMESTAMP NOT NULL DEFAULT NOW()
+    verification_time TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (sender_id) REFERENCES Users(id) ON DELETE SET NULL,
-    FOREIGN KEY (receiver_id) REFERENCES Users(id) ON DELETE SET NULL
+    FOREIGN KEY (receiver_id) REFERENCES Users(id) ON DELETE SET NULL,
+    CHECK (sender_id <> receiver_id)
 );
