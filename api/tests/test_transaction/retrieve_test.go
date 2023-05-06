cd5aa4271cf5995237fd0271382bce5e8ee47534
Daze <dazehere@yandex.com>
2023-05-06 19:34:47 +0545

    feat: transaction retrieve test created

---
+++ api/tests/test_transaction/retrieve_test.go
@@ -0,0 +1,37 @@
+package test_auth
+
+import (
+	"main/endpoints/transaction"
+	. "main/tests/helpers"
+	test "main/tests/helpers"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionRetrieve(t *testing.T) {
+	r, c, w := TestInit()
+
+	retrieveController := transaction.TransactionRetrieve{}
+
+	user := test.CreateLoggedInUser(t, c)
+
+	r.POST("/v1/transaction", retrieveController.Retrieve)
+
+	// Set cookie with user session user.SessionToken
+
+	req, err := http.NewRequest("POST", "/v1/transaction", nil)
+
+	cookie := http.Cookie{Name: "session", Value: user.SessionToken.String()}
+	req.AddCookie(&cookie)
+
+	if err != nil {
+		t.Fatalf("failed to create test request: %v", err)
+	}
+
+	r.ServeHTTP(w, req)
+
+	// Check that the response status code is as expected.
+	assert.Equal(t, http.StatusOK, w.Code)
+}
