91ca0cad75418cd71702e42e46fe7fee99326655
javaclll <yugu.upadhyaya@gmail.com>
2023-05-12 22:49:51 +0545

    feat: transaction qr implemented

--- frontend/lib/components/QRImage.dart
+++ frontend/lib/components/QRImage.dart
@@ -6,20 +6,27 @@ import 'package:qr_flutter/qr_flutter.dart';
 class QRView extends StatelessWidget {
   final Uint8List? data;
   final int errorCorrectLevel;
-
+  final double size;
+  final String stringdata;
   const QRView({
     Key? key,
+    required this.stringdata,
+    this.size = 250,
     this.data,
     this.errorCorrectLevel = QrErrorCorrectLevel.M,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return QrImageView.withQr(
-      qr: QrCode.fromUint8List(
-        data: data ?? Uint8List(0),
-        errorCorrectLevel: errorCorrectLevel,
-      ),
+    return QrImageView(
+      data: stringdata,
+      size: size,
     );
+    // return QrImageView.withQr(
+    //   size: size,
+    //   qr: QrCode.fromUint8List(
+    //     data: data ?? Uint8List(0),
+    //     errorCorrectLevel: errorCorrectLevel,
+    //   ),
   }
 }
--- frontend/lib/screens/QrScan/qrscan_bloc.dart
+++ frontend/lib/screens/QrScan/qrscan_bloc.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:libaaryapay/libaaryapay.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 part 'qrscan_event.dart';
@@ -8,19 +13,59 @@ part 'qrscan_state.dart';
 
 class QrScannerBloc extends Bloc<QrScannerEvent, QrScannerState> {
   final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
-  QRViewController? controller;
 
   QrScannerBloc() : super(const QrScannerState()) {
     on<QrCodeScanned>(_onQrCodeScanned);
+    on<QrScanDataCreate>(_onQrScanDataCreate);
+    on<InitializeScanner>(_onInitializeScanner);
+    on<CloseScanner>(_onCloseScanner);
+    on<PlatformInitializer>(_onPlatformInitializer);
+    add(QrScanDataCreate());
   }
 
-  void _onQrCodeScanned(QrCodeScanned event, Emitter<QrScannerState> emit) {
+  void _onPlatformInitializer(
+      PlatformInitializer event, Emitter<QrScannerState> emit) {
+    QRViewController? newController = state.controller;
+
+    if (Platform.isAndroid) {
+      newController!.pauseCamera();
+    }
+
+    if (Platform.isIOS) {
+      newController!.resumeCamera();
+    }
+
+    emit(state.copyWith(controller: newController));
+  }
+
+  void _onInitializeScanner(
+      InitializeScanner event, Emitter<QrScannerState> emit) {
+    emit(QrScannerState(controller: event.controller));
+  }
+
+  void _onCloseScanner(CloseScanner event, Emitter<QrScannerState> emit) {
+    state.controller?.dispose();
+  }
+
+  void _onQrScanDataCreate(
+      QrScanDataCreate event, Emitter<QrScannerState> emit) async {
+    var qrData = await generateQRData();
+    emit(state.copyWith(qrData: qrData));
+  }
+
+  void _onQrCodeScanned(
+      QrCodeScanned event, Emitter<QrScannerState> emit) async {
+    print("This is Scanned ${event.code}");
+    print("This is Scanned ${utf8.decode(event.code)}");
+
     emit(state.copyWith(code: event.code));
   }
 
-  @override
-  Future<void> close() {
-    controller!.dispose();
-    return super.close();
+  Future<String> generateQRData() async {
+    final serverKeyPair = keyPairFromBase64(
+      "fzAng9P5nxQCaxh4+sExTCdRI2++KmwBRKohfBJ8RuuD5sb/gfzu1BrFiJeGJudEOwAp1ZVekbVwWrLmRlzu1g==",
+    );
+
+    return "Hello";
   }
 }
